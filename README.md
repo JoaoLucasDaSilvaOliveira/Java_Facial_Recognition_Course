de1acd3e1ce673748c94fbcd923878e4618386a6
Joao Lucas <dev_jao@dev.jao>
2026-03-04 02:19:17 -0300

    first commti

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java Facial Recognition Course
---
+++ src/main/java/org/facial_recognition/javacourse/Main.java
@@ -0,0 +1,8 @@
+package org.facial_recognition.javacourse;
+
+public class Main 
+{
+    public static void main( String[] args ) {
+        System.out.println("Hello");
+    }
+}
